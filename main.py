6c8814ca7b67ad35994a3e808c0c7d20a1f0b6a6
goosegusgo <goosegusgo@proton.me>
2022-08-29 01:09:49 +0300

    2

--- main.py
+++ main.py
@@ -20,10 +20,17 @@ class FlatIterator:
 		self.index += 1
 		return self.list[self.index]
 
-
+def flat_generator(flat_list):
+	ind = 0
+	flat_list = [x for l in flat_list for x in l]
+	while len(flat_list) > ind:
+		yield flat_list[ind]
+		ind += 1
 
 if __name__ == '__main__':
 	for item in FlatIterator(nested_list):
 	    print(item)
 	flat_list = [item for item in FlatIterator(nested_list)]
 	print(flat_list)
+	for item in  flat_generator(nested_list):
+		print(item)
